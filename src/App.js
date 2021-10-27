7ee8f54b9eb78f138dcee013c80c58f78f9a84bf
joshua-anderson-ext <79172763+joshua-anderson-ext@users.noreply.github.com>
2021-10-27 16:38:39 -0400

    Initial Upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './styles/style.css';
+import Header from './Header.js';
+import Footer from './Footer.js';
+import Dashboard from './components/dashboard.js';
+import Amplify, { Auth } from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+
+Amplify.configure(awsconfig);
+
+//https://docs.amplify.aws/lib/auth/getting-started/q/platform/js/#option-1-use-pre-built-ui-components
+
+function App() {
+  return (
+    <div className="App h-100">
+      <AmplifySignOut />
+      <Header/>
+      <main className="d-flex flex-column justify-content-between">
+        <Dashboard/>
+        <Footer/>
+      </main>
+    </div>
+  );
+}
+
+export default withAuthenticator(App);
